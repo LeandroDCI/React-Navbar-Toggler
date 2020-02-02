382f8404f301f90bc971283b7f37f4a1bd92e0ca
Leandro <leandro.frigerio@devugees.org>
2020-02-02 13:18:10 +0100

    Footer content

--- src/views/Footer.js
+++ src/views/Footer.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer>
-            <small>Created with &hearts; in Berlin with React | Copyright {new Date().getFullYear()} <a href="https://github.com/LeandroDCI">Leandro</a></small>
+            <small>
+                Created with &hearts; in Berlin with React |
+                Copyright {new Date().getFullYear()} <a href="https://github.com/LeandroDCI">Leandro</a></small>
         </footer>
     )
 }
