2c5b4f960bc673e423bca0f9eace00c66b29ded9
Leandro <leandro.frigerio@devugees.org>
2020-02-03 11:14:13 +0100

    userAuth faker

---
+++ src/modules/userAuth.js
@@ -0,0 +1,8 @@
+const userAuth = async () => {
+    return {
+        id: 0,
+        name: "Leandro",
+        email: "leandro.frigrio@digitalcareerinstitute.org"
+    };
+};
+export default userAuth;
